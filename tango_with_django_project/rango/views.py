8ca292d6ccbb7ef4c1ea4dd13efaca12aceca877
GLunPy <areco.eduardo@gmail.com>
2015-07-28 16:54:13 -0400

    7.3.2 slugify urls. cansado ya, pero sigamos

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 def index(request):
-	context_dict = {'boldmessage': "Soy un mensaje en negrita"}
+	category_list = Category.objects.order_by('likes')[:5]
+	context_dict = {'categories': category_list}
 	return render(request, 'rango/index.html',context_dict)
 # Create your views here.
