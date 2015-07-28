44b1cf4e6711b7facac3185e14458c6863b934aa
GLunPy <areco.eduardo@gmail.com>
2015-07-27 22:36:39 -0400

    capitulo sobre templates en httwd

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
+def index(request):
+	context_dict = {'boldmessage': "Soy un mensaje en negrita"}
+	return render(request, 'rango/index.html',context_dict)
 # Create your views here.
